e33cbfc0aff6a498923821823fc3dead53716dfd
Emanuel Neto <ti.emanuelneto@yahoo.com.br>
2020-07-30 00:13:09 -0300

    aplicação para calculo rapido de notas de alguns estudantes

---
+++ GradingStudents/GradingStudents/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradingStudents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gradesCount = Convert.ToInt32(Console.ReadLine().Trim()); // 4
+            List<int> grades = new List<int>();
+
+            for (int i = 0; i < gradesCount; i++)
+            {
+                int gradesItem = Convert.ToInt32(Console.ReadLine().Trim()); // 73 67 38 33
+                grades.Add(gradesItem);
+            }
+
+            List<int> result = Result.gradingStudents(grades);
+
+            Console.WriteLine(String.Join("\n", result));
+            Console.ReadKey();
+        }
+    }
+
+    class Result
+    {
+        public static List<int> gradingStudents(List<int> grades)
+        {
+            var gradesRevised = new List<int>();
+            
+            if (grades.Count == 0)
+                return grades;
+            
+            foreach (var grade in grades)
+            {
+                var mod = (grade + 2) % 5;
+                
+                if(mod >= 0 && mod <= 2 && grade > 37)
+                    gradesRevised.Add(grade + (2 - mod));
+                else
+                    gradesRevised.Add(grade);
+            }
+
+            return gradesRevised;
+        }
+    }
+}
