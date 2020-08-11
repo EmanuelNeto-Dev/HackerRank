985c69e717912165ff5802b987609682b18046ed
Emanuel Neto <ti.emanuelneto@yahoo.com.br>
2020-08-10 23:14:24 -0300

    Kangaroo chalenge

---
+++ Kangaroo/Kangaroo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kangaroo
+{
+    class Program
+    {
+        static string kangaroo(int x1, int v1, int x2, int v2)
+        {
+            var result = "NO";
+            if (v1 > v2)
+            {
+                if ((x1 - x2) % (v2 - v1) == 0)
+                {
+                    result = "YES";
+                }
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] x1V1X2V2 = Console.ReadLine().Split(' ');
+
+            int x1 = Convert.ToInt32(x1V1X2V2[0]);
+
+            int v1 = Convert.ToInt32(x1V1X2V2[1]);
+
+            int x2 = Convert.ToInt32(x1V1X2V2[2]);
+
+            int v2 = Convert.ToInt32(x1V1X2V2[3]);
+
+            string result = kangaroo(x1, v1, x2, v2);
+
+            Console.WriteLine($"{result}");
+        }
+    }
+}
