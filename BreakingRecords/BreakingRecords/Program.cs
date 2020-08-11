08274238bf63880858753060cb44dc8cb81f4266
Emanuel Neto <ti.emanuelneto@yahoo.com.br>
2020-08-11 20:16:42 -0300

    HackerRank Challenge - Breaking Records

---
+++ BreakingRecords/BreakingRecords/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakingRecords
+{
+    class Program
+    {
+        // Complete the breakingRecords function below.
+        static int[] breakingRecords(int[] scores)
+        {
+            int highestScore = 0;
+            int lowestScore = 0;
+            int counterMax = 0;
+            int counterMin = 0;
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+
+                if (i == 0)
+                {
+                    highestScore = scores[i];
+                    lowestScore = scores[i];
+                    continue;
+                }
+
+                if (scores[i] > highestScore)
+                {
+                    highestScore = scores[i];
+                    counterMax++;
+                }
+                else if (scores[i] < lowestScore)
+                {
+                    lowestScore = scores[i];
+                    counterMin++;
+                }
+
+            }
+
+            int[] breakedRecords = { counterMax, counterMin };
+            return breakedRecords;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] scores = Array.ConvertAll(Console.ReadLine().Split(' '), scoresTemp => Convert.ToInt32(scoresTemp));
+            int[] result = breakingRecords(scores);
+
+            Console.WriteLine(string.Join(" ", result));
+
+        }
+    }
+}
