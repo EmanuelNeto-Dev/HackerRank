1f42e41841dcc83d052bb8ea1364c1a6e942f886
Emanuel Neto <ti.emanuelneto@yahoo.com.br>
2020-08-12 23:59:15 -0300

    HackerRank Challenge - Migratory Birds

---
+++ MigratoryBirds/MigratoryBirds/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigratoryBirds
+{
+    class Program
+    {
+        // Complete the migratoryBirds function below.
+        static int migratoryBirds(List<int> arr)
+        {
+
+            var query = from bird
+                        in arr
+                        group bird by bird into g
+                        let count = g.Count()
+                        orderby count descending
+                        select g.Key;
+
+            return query.FirstOrDefault();
+
+            //Alternative: 
+            /*
+                static void Main(String[] args)
+                {
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    string[] types_temp = Console.ReadLine().Split(' ');
+                    int[] types = Array.ConvertAll(types_temp, Int32.Parse);
+
+                    int[] birdCounts = new int[6];
+
+                    foreach (var type in types)
+                        birdCounts[type]++;
+
+                    int maxValue = birdCounts.Max();
+                    int maxType = birdCounts.ToList().IndexOf(maxValue);
+
+                    Console.WriteLine(maxType);
+                }
+            */
+        }
+
+        static void Main(string[] args)
+        {
+            int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            int result = migratoryBirds(arr);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
