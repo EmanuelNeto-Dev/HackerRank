82dd2c44b2dfc81ad81c9ecd74f5f88b2053b04c
Emanuel Neto <ti.emanuelneto@yahoo.com.br>
2020-08-15 22:51:05 -0300

    HackerRank Challenge - Day of Programmer

---
+++ DayOfProgrammer/DayOfProgrammer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayOfProgrammer
+{
+    class Program
+    {
+        // Complete the dayOfProgrammer function below.
+        static string dayOfTheProgrammer(int year)
+        {
+            if (year == 1918)
+                return "26.09.1918";
+            else if (((year <= 1917) && (year % 4 == 0)) || ((year > 1918) && (year % 400 == 0 || ((year % 4 == 0) && (year % 100 != 0)))))
+                return $"12.09.{year}";
+            else
+                return $"13.09.{year}";
+
+        }
+
+        static void Main(string[] args)
+        {
+            int year = Convert.ToInt32(Console.ReadLine().Trim());
+
+            string result = dayOfTheProgrammer(year);
+
+            Console.WriteLine(result);
+        }
+    }
+}
