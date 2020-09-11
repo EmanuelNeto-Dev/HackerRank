97805bd69435edfd85f20ad16411d7247be95c92
Emanuel Neto <ti.emanuelneto@yahoo.com.br>
2020-09-10 21:39:59 -0300

    HackerRank Challenge - Find Digits

---
+++ FindDigits/FindDigits/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDigits
+{
+    class Program
+    {
+        static int findDigits(int n)
+        {
+            int digits = 0;
+            string digitConverted = n.ToString();
+
+            for (int i = 0; i < digitConverted.Length; i++)
+            {
+                if (digitConverted[i].Equals('0'))
+                    continue;
+
+                if (n % int.Parse(digitConverted[i].ToString()) == 0)
+                    digits++;
+            }
+
+            return digits;
+        }
+
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                int result = findDigits(n);
+                Console.WriteLine(result);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
