9fe1b694bc3f8db45f7e187ef90756229e66f94b
Emanuel Neto <ti.emanuelneto@yahoo.com.br>
2020-09-22 16:59:10 -0300

    HackerRank Challenge - Jumping on the clouds 2

---
+++ JumpingOnTheClouds_2/JumpingOnTheClouds_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpingOnTheClouds_2
+{
+    class Program
+    {
+        static int jumpingOnClouds(int[] c)
+        {
+            int jumps = 0;
+            
+            int position = 0;
+            while (position < c.Length - 1)
+            {
+                if (position + 1 == c.Length - 1)
+                    position += 1;
+                else if (c[position + 2] == 1)
+                    position += 1;
+                else
+                    position += 2;
+
+                jumps++;
+            }
+
+            return jumps;
+        }
+
+        static void Main(string[] args)
+        {
+            //int n = Convert.ToInt32(Console.ReadLine());
+            int n = Convert.ToInt32("10");
+
+            //int[] c = Array.ConvertAll("0 0 0 0 1 0".Split(' '), cTemp => Convert.ToInt32(cTemp));
+            //int[] c = Array.ConvertAll("0 0 1 0 0 1 0".Split(' '), cTemp => Convert.ToInt32(cTemp));
+            int[] c = Array.ConvertAll("0 0 1 0 0 0 0 1 0 0".Split(' '), cTemp => Convert.ToInt32(cTemp));
+            int result = jumpingOnClouds(c);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
